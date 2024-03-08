a95380cb43ff5e1bec543a086e95d13b48ae1135
Vp_pDoochi <wow2658@naver.com>
2024-03-09 01:58:42 +0900

    TIL: exception try if throw catch

---
+++ Exception/19_예외처리 try throw catch.cpp
@@ -0,0 +1,45 @@
+﻿// 에러(err) : 의도치 않게 프로그램이 비정상 종료가 되는 것
+//				하드웨어의 잘못된 동작, 고장으로 인한 오류, 정전, 배드섹터, 블루스크린, 0으로 나누기, 배열 범위 실수 등이 있다.
+// 예외(exception) : 사용자의 잘못된 조작 or 프로그래밍으로 오류가 발생해 프로그램이 종료가 되는 것.
+// 예외처리를 추가해주면 정상 실행 상태로 되돌릴 수가있다.
+// 즉, 예외 처리의 목적은 프로그램을 정상 실행 상태로 돌리는 것이다!
+// 일반 예외(컴파일 예외, checked exception) : 빨간줄, 잘못된 문법으로 컴파일 당시에 예외가 발생하는 것
+// 실행 예외(런타임 예외, unchecked exception) : 예외 처리 코드를 생략해도 컴파일은 되는 예외 (경험 및 노하우에 따라 예외 처리 코드 필요)
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int pizze_slice = 0;
+	int persons = 0;
+	int slices_persons = 0;
+
+	try
+	{
+		cout << "피자 조각 수를 입력하시오 : ";
+		cin >> pizze_slice;
+		if (0 >= pizze_slice)
+		{
+			throw persons;
+		}
+		cout << "사람 수를 입력하시오 : ";
+		cin >> persons;
+		if (0 >= persons)
+		{
+			throw persons;
+		}
+	}
+	//catch (...) 모든 경우에서 받고 싶을때
+	catch (int e) // 타입이 꼭 일치해야한다. persons를 throw하니까 int로 받을 준비.
+	{
+		cout << "예외 발생" << endl;
+	}
+
+
+	slices_persons = pizze_slice / persons;
+	cout << "한 사람당 피자는 : " << slices_persons << "조각입니다." << endl;
+
+	return 0;
+
+}
---
+++ Exception/20_예외처리 문자열 throw string.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+	string str ="";
+	cout << "가위, 바위, 보 게임입니다.: "<<endl;
+	try
+	{
+		cout << "무엇을 내시겠습니까? : ";
+		cin >> str;;
+		if ("가위"!= str && "바위" != str && "보" != str)
+		{
+			throw "\"가위\", \"바위\", \"보\" 중에서만 선택해주세요.";
+		}
+	}
+	catch (const char* message)
+	{
+		cout << "예외 발생 : " << message << endl;
+	}
+	cout<<"당신은 "<<str <<"을 내셨습니다." << endl;
+
+	return 0;
+
+}
